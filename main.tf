7ffc702b72864b29dd6604456dbe1d0b357763d8
Daniel Borner <71730916+DBorner@users.noreply.github.com>
2024-04-03 13:21:53 +0200

    Add terraform file

--- main.tf
+++ main.tf
@@ -1,84 +1,181 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  default     = "key_ssh.pem"
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+}
+
+resource "aws_instance" "app_server" {
+  ami                         = "ami-0c101f26f147fa7fd"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.pub_subnet.id
+  vpc_security_group_ids      = [aws_security_group.my_group.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.key_pair.key_name
+
+  tags = {
+    Name = "TTTServer"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file(var.key_name)
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y docker",
+      "sudo usermod -aG docker $${USER}",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      "sudo yum install -y git",
+      "sudo git clone https://github.com/DBorner/PWr-PwCh.git",
+      "echo 'API_URL=\"http://${self.public_ip}:3000/api\"' | sudo tee PWr-PwCh/frontend/.env.local",
+      "echo 'SOCKET_URL=\"http://${self.public_ip}:3000\"' | sudo tee -a PWr-PwCh/frontend/.env.local",
+      "cd PWr-PwCh",
+      "sudo docker-compose up --detach"
+    ]
+  }
+}
+
+
+resource "aws_security_group" "my_group" {
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "brontend"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "backend"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
   tags = {
     Name = "MyVPC"
   }
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_subnet" "pub_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  tags = {
+    Name = "My public subnet"
+  }
 }
 
-resource "aws_internet_gateway" "my_gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "My IGW"
+  }
 }
 
-resource "aws_route_table" "my_route_table" {
+resource "aws_route_table" "pub_rt" {
   vpc_id = aws_vpc.my_vpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_gw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
-resource "aws_route_table_association" "a" {
-  subnet_id = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_route_table.id
+resource "aws_route_table_association" "pub_rt_association" {
+  subnet_id      = aws_subnet.pub_subnet.id
+  route_table_id = aws_route_table.pub_rt.id
 }
 
-resource "aws_security_group" "my_sg" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "null_resource" "change_key_permissions" {
+  provisioner "local-exec" {
+    command = "chmod 400 ${var.key_name}"
   }
 
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "MySecurityGroup"
+  triggers = {
+    key_name = aws_key_pair.key_pair.key_name
   }
 }
 
-resource "aws_instance" "my_instance" {
-  ami = "ami-0b641050bf042aa4a"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.my_subnet.id
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
-
-  tags = {
-    Name = "MyInstance"
-  }
+output "instance_public_ip" {
+  value = aws_instance.app_server.public_ip
 }
